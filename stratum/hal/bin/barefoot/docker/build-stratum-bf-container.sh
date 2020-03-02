f36fc2352faa8e8debea8bfd5d1fca938341ae49
Yi Tseng <yi@opennetworking.org>
2020-03-02 14:36:12 -0800

    Add Stratum container without kernel module

--- stratum/hal/bin/barefoot/docker/build-stratum-bf-container.sh
+++ stratum/hal/bin/barefoot/docker/build-stratum-bf-container.sh
@@ -17,14 +17,17 @@
 
 STRATUM_ROOT=${STRATUM_ROOT:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../../.." >/dev/null 2>&1 && pwd )"}
 JOBS=${JOBS:-4}
+BUILD_ARGS="--build-arg JOBS=$JOBS"
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 SDE_TAR KERNEL_HEADERS_TAR"
+if [ "$#" -eq 1 ]; then
+    BUILD_ARGS = "$BUILD_ARGS --build-arg SDE_TAR=$1"
+elif [ "$#" -eq 2 ]; then
+    BUILD_ARGS = "$BUILD_ARGS --build-arg SDE_TAR=$1 --build-arg KERNEL_HEADERS_TAR=$2"
+else
+    echo "Usage: $0 SDE_TAR [KERNEL_HEADERS_TAR]"
     exit 1
 fi
 
 docker build -t stratumproject/stratum-bf \
-               --build-arg JOBS=$JOBS \
-               --build-arg SDE_TAR=$1 \
-               --build-arg KERNEL_HEADERS_TAR=$2 \
+                $BUILD_ARGS \
                -f $STRATUM_ROOT/stratum/hal/bin/barefoot/docker/Dockerfile $STRATUM_ROOT
